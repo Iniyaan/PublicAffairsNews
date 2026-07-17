214b1185841effac57893d78772491c61b07a1bd
Iniyaan <127408977+Iniyaan@users.noreply.github.com>
2026-07-17 11:03:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+==========================================================
+Public Affairs Daily Intelligence Portal
+Version 1.0
+==========================================================
+*/
+
+console.log("Portal Loaded");
+
+// Future Features
+
+// Search
+
+// Filter
+
+// Sort
+
+// Dark Mode
+
+// Charts
